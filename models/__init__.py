ad2f7c1bc2edc36ac9cd70de944c3275d773db48
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-22 16:21:30 +0530

    update: model registery

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,20 @@
 from .ddpm import DDPM2D
 from .flow_matching import FlowMatching2D
+from .flow_matching_images import FlowMatchingImage
+from .flow_matching_conditional import ConditionalFlowMatching
 from .networks import SimpleMLP
 from .base import DiffusionModel
 from .unet import UNet
+from .unet_conditional import ConditionalUNet
 
-__all__ = ["DDPM2D", "FlowMatching2D", "SimpleMLP", "DiffusionModel", "UNet"]
+
+__all__ = [
+    "DDPM2D",
+    "FlowMatching2D",
+    "FlowMatchingImage",
+    "ConditionalFlowMatching",
+    "SimpleMLP",
+    "UNet",
+    "ConditionalUNet",
+    "DiffusionModel",
+]
